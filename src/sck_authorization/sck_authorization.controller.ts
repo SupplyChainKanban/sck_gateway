b836f4cd0560863ea1669507a96a32e9da781e61
Slater <armando.jaimes@pucp.edu.pe>
2024-09-10 21:41:47 -0500

    Agregar microservicio de autenticación

--- src/sck_authorization/sck_authorization.controller.ts
+++ src/sck_authorization/sck_authorization.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { SCK_NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('sck-authorization')
 export class SckAuthorizationController {
@@ -10,16 +11,16 @@ export class SckAuthorizationController {
   ) { }
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {})
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto)
       .pipe(
         catchError(err => { throw new RpcException(err) })
       )
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {})
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto)
       .pipe(
         catchError(err => { throw new RpcException(err) })
       )
